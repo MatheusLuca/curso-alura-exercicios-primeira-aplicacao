75894a5a7eb9a6c6785368282219b4af8d74e86c
Matheus Luca <luca_matheus@live.com>
2026-04-19 16:27:22 -0300

    Feat: Atualização git ignore / criacao e desenvolvimento do main e exercicio

---
+++ src/Exercicio1.java
@@ -0,0 +1,10 @@
+public class Exercicio1 {
+    static void main(String[] args) {
+
+        double celsius = 32.0;
+
+        double fahrenheit = (celsius * 1.8) + 32;
+        System.out.println(fahrenheit);
+
+    }
+}
